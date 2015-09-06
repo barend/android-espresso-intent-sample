21026b936fa2f57cb651d54d487066a950f45a71
Barend Garvelink <bgarvelink@xebia.com>
2015-09-06 16:21:56 +0200

    Adds simple rule-based Espresso test.

---
+++ app/src/androidTest/java/nl/garvelink/blog/espressointentsample/MainActivityTest.java
@@ -0,0 +1,47 @@
+/*
+ *     Copyright 2015 Barend Garvelink
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package nl.garvelink.blog.espressointentsample;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * This Espresso test uses the (fairly) new {@code ActivityTestRule} and doesn't have to extend
+ * {@code ActivityInstrumentationTestCase2} like you used to have to do.
+ */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void shouldShowHelloWorldForEmptyIntent() {
+        onView(withId(R.id.main__tv_hello)).check(matches(withText("Hello world!")));
+    }
+}
